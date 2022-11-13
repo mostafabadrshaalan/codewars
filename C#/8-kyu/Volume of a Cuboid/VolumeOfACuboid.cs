f0666ba490c9780c1683de37c27df5a9a1c792e2
mostafabadrshaalan <moustafabadr.shaalan@gmail.com>
2022-11-13 14:30:23 +0200

    ADD New Problem

---
+++ C#/8-kyu/Volume of a Cuboid/VolumeOfACuboid.cs
@@ -0,0 +1,8 @@
+public class Kata
+{
+    public static double getVolumeOfCubiod(double length, double width, double height)
+    {
+        double vol = length * width * height;
+        return vol;
+    }
+}
