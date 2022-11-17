8049a702461470f74ccb78cc0bf530fb5774e345
mostafabadrshaalan <moustafabadr.shaalan@gmail.com>
2022-11-17 17:38:47 +0200

    Edit Code Volume

--- C#/8-kyu/5-Volume of a Cuboid/VolumeOfACuboid.cs
+++ C#/8-kyu/5-Volume of a Cuboid/VolumeOfACuboid.cs
@@ -2,7 +2,7 @@ public class Kata
 {
     public static double getVolumeOfCubiod(double length, double width, double height)
     {
-        double vol = length * width * height;
-        return vol;
+        return length * width * height;
+        
     }
 }
